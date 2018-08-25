5c9d75d681632de2a794b1b35e2ab910b1db3202
pindhu <p.indhu@gmail.com>
2018-08-25 15:28:38 -0700

    Changes to host on github

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,9 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    //return `http://localhost:${port}/data/restaurants.json`;
+    return `https://github.com/p-indhu/restaurant-reviews-stage1/data/restaurants.json`;
+
   }
 
   /**
@@ -150,14 +152,14 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
+    // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
       alt: restaurant.name,
@@ -165,7 +167,7 @@ class DBHelper {
       })
       marker.addTo(newMap);
     return marker;
-  } 
+  }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
@@ -178,4 +180,3 @@ class DBHelper {
   } */
 
 }
-
